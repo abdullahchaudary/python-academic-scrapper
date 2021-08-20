ac05f696254128b031b7113492956a147a455f38
Abdullah Chaudary <abchaudary@outlook.com>
2021-08-20 19:26:18 +0500

    kivy ui dev started

--- kivy/main.py
+++ kivy/main.py
@@ -2,12 +2,18 @@ from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 # from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
 # from kivy.uix.widget import Widget
 # from kivy.uix.button import Button
 from kivy.config import Config
 
 Config.set('graphics', 'resizable', True)
 
+
+class AcademicScraper:
+    def generate_boxes():
+        print("generating Boxes!!!")
 
 class TopSection(BoxLayout):
     pass
@@ -21,5 +27,9 @@ class AcademicWebScraperApp(App):
     def build(self):
         # return super().build()
         return MainUI()
+
+    def runScrape(self, keyword):
+        print("hello ", keyword)
+        AcademicScraper.generate_boxes()
 
 AcademicWebScraperApp().run()
